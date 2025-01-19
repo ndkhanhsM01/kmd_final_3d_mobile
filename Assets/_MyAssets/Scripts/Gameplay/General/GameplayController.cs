d92ae2b33ce268ad8307f08c4d8508d7b02f593c
ndkhanhs_m01 <mainver01@gmail.com>
2025-01-19 20:53:46 +0700

    [Gamepla] common events

--- Assets/_MyAssets/Scripts/Gameplay/General/GameplayController.cs
+++ Assets/_MyAssets/Scripts/Gameplay/General/GameplayController.cs
@@ -8,6 +8,12 @@ public class GameplayController : MSingleton<GameplayController>
     [SerializeField] private InputHandler input;
     [SerializeField] private LevelLoader levelLoader;
 
+    [Space(20f)]
+    [Header("Event channels")]
+    [SerializeField] private SOVoidEventChannel channelStart;
+    [SerializeField] private SOVoidEventChannel channelWin;
+    [SerializeField] private SOVoidEventChannel channelLose;
+
     private Level curLevel;
     private MainCharacter mc;
     public MainCharacter MC => mc;
@@ -22,5 +28,18 @@ public class GameplayController : MSingleton<GameplayController>
         curLevel = levelLoader.LoadLevel();
 
         mc = curLevel.MC;
+    }
+
+    public void StartPlay()
+    {
+        channelStart.Raise();
+    }
+    public void WinLevel()
+    {
+        channelWin.Raise();
+    }
+    public void LoseLevel()
+    {
+        channelLose.Raise();
     }
 }
