7de81e84b0537c26d581503886b911342acc8fc9
ndkhanhs_m01 <mainver01@gmail.com>
2025-01-21 21:01:44 +0700

    input handler

--- Assets/_MyAssets/Scripts/Gameplay/Player/InputHandler.cs
+++ Assets/_MyAssets/Scripts/Gameplay/Player/InputHandler.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    [Space(10f)]
     [Header("Input")]
     [SerializeField] private Joystick joystick;
     [SerializeField] private SpecialSelection specialSelection;
 
-    [Space(20f)]
+    [Space(10f)]
     [Header("Shared variable")]
     [SerializeField] private SOVector3Variable sharedMoveDirection;
+
+    [Space(10f)]
+    [Header("Events")]
+    [SerializeField] private SOVoidEventChannel tapChannel;
+
+
+    private void OnEnable()
+    {
+        specialSelection.OnTap += OnTapSpecial;
+        specialSelection.OnBeginHolding += OnBeginHoldSpecial;
+        specialSelection.OnEndHolding += OnEndHoldSpecial;
+        specialSelection.OnReleased += OnReleasedSpecial;
+    }
+    private void OnDisable()
+    {
+        specialSelection.OnTap -= OnTapSpecial;
+        specialSelection.OnBeginHolding -= OnBeginHoldSpecial;
+        specialSelection.OnEndHolding -= OnEndHoldSpecial;
+        specialSelection.OnReleased -= OnReleasedSpecial;
+    }
 
     private void Update()
     {
         sharedMoveDirection.Value.x = joystick.Horizontal;
         sharedMoveDirection.Value.y = joystick.Vertical;
+    }
+
+    private void OnTapSpecial()
+    {
+        Debug.Log("Tap");
+    }
+    private void OnBeginHoldSpecial() 
+    {
+        Debug.Log("Begin hold");
+    }
+    private void OnEndHoldSpecial()
+    {
+        Debug.Log("End hold");
+    }
+    private void OnReleasedSpecial()
+    {
+        Debug.Log("Released");
     }
 }
--- Assets/_MyAssets/Scripts/Gameplay/Player/SpecialSelection.cs
+++ Assets/_MyAssets/Scripts/Gameplay/Player/SpecialSelection.cs
@@ -1,18 +1,81 @@
 
 
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SpecialSelection : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    [Header("Configure")]
+    [SerializeField, Min(0f)] private float minTimeToHold = 0.15f;
+
+    public Action OnTap;
+    public Action OnBeginHolding;
+    public Action OnEndHolding;
+    public Action OnReleased;
+
+    private bool isPressed;
+    private bool isHolding;
+
+    public bool IsHolding => isHolding;
+
+    private Coroutine crCheckHolding;
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("SpecialSelection down");
+        isPressed = true;
+
+        StartCheckHolding();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("SpecialSelection up");
+        if (isHolding)
+            OnEndHolding?.Invoke();
+
+        isHolding = false;
+        isPressed = false;
+
+        OnReleased?.Invoke();
+    }
+
+    private void StartCheckHolding()
+    {
+        StopCheckHolding();
+        crCheckHolding = StartCoroutine(IE_CheckHolding());
+    }
+
+    private void StopCheckHolding()
+    {
+        if(crCheckHolding != null)
+            StopCoroutine(crCheckHolding);
+    }
+
+    private IEnumerator IE_CheckHolding()
+    {
+        if(!isPressed)
+            yield break;
+
+        float timer = 0f;
+        isHolding = false;
+
+        while(timer <= minTimeToHold)
+        {
+            if(!isPressed)
+            {
+                OnTap?.Invoke();
+                yield break;
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        if(isPressed)
+        {
+            OnBeginHolding?.Invoke();
+            isHolding = true;
+        }
     }
 }
